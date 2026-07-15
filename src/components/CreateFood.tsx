164f3dbd13607be4ee8e3fb197a06b665b7e4520
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-07-15 15:43:44 -0400

    Adopt repository: src/components/CreateFood.tsx

---
+++ src/components/CreateFood.tsx
@@ -0,0 +1,48 @@
+import { useMachine } from "@xstate/react";
+import { machine } from "~/machines/manage-food";
+import { Button } from "./Button";
+import { Dialog, DialogTrigger } from "./Dialog";
+import FoodEditing from "./FoodEditing";
+import { Modal, ModalOverlay } from "./Modal";
+
+export default function CreateFood() {
+  const [snapshot, send] = useMachine(machine, { input: undefined });
+  return (
+    <DialogTrigger>
+      <Button className="w-full">Create food</Button>
+      <ModalOverlay isDismissable>
+        <Modal>
+          <Dialog>
+            {({ close }) => (
+              <FoodEditing
+                brandActor={snapshot.context.brand}
+                caloriesActor={snapshot.context.calories}
+                carbohydratesActor={snapshot.context.carbohydrates}
+                fibersActor={snapshot.context.fibers}
+                nameActor={snapshot.context.name}
+                proteinsActor={snapshot.context.proteins}
+                saltActor={snapshot.context.salt}
+                sugarsActor={snapshot.context.sugars}
+                fatsActor={snapshot.context.fats}
+                fatsSaturatedActor={snapshot.context.fatsSaturated}
+                onSubmit={() => send({ type: "food.create" })}
+              >
+                <Button
+                  action="update"
+                  type="submit"
+                  isDisabled={snapshot.matches("Creating")}
+                >
+                  {snapshot.matches("Creating") ? "Creating..." : "Create"}
+                </Button>
+
+                {snapshot.context.submitError !== null && (
+                  <p>{snapshot.context.submitError}</p>
+                )}
+              </FoodEditing>
+            )}
+          </Dialog>
+        </Modal>
+      </ModalOverlay>
+    </DialogTrigger>
+  );
+}
