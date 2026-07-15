c0fc8f986004dd031940d0a212fca4e8f869eeb9
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-07-15 15:44:30 -0400

    Adopt repository: src/schema/daily-log.ts

---
+++ src/schema/daily-log.ts
@@ -0,0 +1,38 @@
+import { DateTime, Schema } from "effect";
+import { PrimaryKeyIndex } from "./shared";
+
+export class DailyLogInsert extends Schema.Class<DailyLogInsert>(
+  "DailyLogInsert"
+)({
+  date: Schema.DateTimeUtcFromSelf,
+  planId: PrimaryKeyIndex,
+}) {}
+
+export class DailyLogSelect extends Schema.Class<DailyLogSelect>(
+  "DailyLogSelect"
+)({
+  date: Schema.DateTimeUtc,
+  planId: PrimaryKeyIndex,
+}) {
+  static readonly formatDate = DateTime.formatIsoDateUtc;
+  static readonly displayDate = (date: DateTime.Utc) => {
+    const nowDay = DateTime.toParts(DateTime.unsafeNow()).day;
+    const dateDay = DateTime.toParts(date).day;
+    if (nowDay === dateDay) {
+      return "Today";
+    } else {
+      return DateTime.format(date, {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      });
+    }
+  };
+}
+
+export class DailyLogUpdate extends Schema.Class<DailyLogUpdate>(
+  "DailyLogUpdate"
+)({
+  date: Schema.DateTimeUtcFromSelf,
+  planId: PrimaryKeyIndex,
+}) {}
