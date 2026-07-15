1ae731c377ddc861c489200c82c951410c285871
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-07-15 15:43:58 -0400

    Adopt repository: src/components/SelectFood.tsx

---
+++ src/components/SelectFood.tsx
@@ -0,0 +1,71 @@
+import { Either, Match } from "effect";
+import { ListPlusIcon } from "lucide-react";
+import { Button as AriaButton, Group } from "react-aria-components";
+import { useFoods } from "~/hooks/use-foods";
+import type { DailyLogSelect } from "~/schema/daily-log";
+import type { Meal } from "~/schema/shared";
+import CreateFood from "./CreateFood";
+import CreateServing from "./CreateServing";
+import { Dialog, DialogTrigger } from "./Dialog";
+import { Modal, ModalOverlay } from "./Modal";
+import UpdateFood from "./UpdateFood";
+
+export default function SelectFood({
+  meal,
+  dailyLogDate,
+}: {
+  meal: typeof Meal.Type;
+  dailyLogDate: typeof DailyLogSelect.fields.date.Type;
+}) {
+  const foods = useFoods();
+  return (
+    <DialogTrigger>
+      <AriaButton className="focus:outline-none">
+        <ListPlusIcon />
+      </AriaButton>
+      <ModalOverlay isDismissable>
+        <Modal>
+          <Dialog>
+            {({ close }) => (
+              <div className="flex flex-col gap-y-6">
+                <div className="flex flex-col gap-y-2">
+                  {Either.match(foods, {
+                    onLeft: Match.valueTags({
+                      MissingData: () => <p>No foods found</p>,
+                      InvalidData: ({ parseError }) => (
+                        <p>
+                          Invalid data: {JSON.stringify(parseError, null, 2)}
+                        </p>
+                      ),
+                    }),
+                    onRight: (_) => (
+                      <>
+                        {_.map((food) => (
+                          <div
+                            key={food.id}
+                            className="flex items-center justify-between"
+                          >
+                            <p className="font-medium">{food.name}</p>
+                            <Group className="flex items-center justify-end gap-x-2">
+                              <UpdateFood food={food} />
+                              <CreateServing
+                                dailyLogDate={dailyLogDate}
+                                meal={meal}
+                                foodId={food.id}
+                              />
+                            </Group>
+                          </div>
+                        ))}
+                      </>
+                    ),
+                  })}
+                </div>
+                <CreateFood />
+              </div>
+            )}
+          </Dialog>
+        </Modal>
+      </ModalOverlay>
+    </DialogTrigger>
+  );
+}
