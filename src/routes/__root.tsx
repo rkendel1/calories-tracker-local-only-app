91fb782c14dc3946a01337277a8e357e7872ee98
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-07-15 15:44:27 -0400

    Adopt repository: src/routes/__root.tsx

---
+++ src/routes/__root.tsx
@@ -0,0 +1,24 @@
+import { PGliteProvider } from "@electric-sql/pglite-react";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { PgliteDrizzleContext } from "~/hooks/use-pglite-drizzle";
+import { Pglite } from "~/services/pglite";
+import { RuntimeClient } from "~/services/runtime-client";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+  loader: () => RuntimeClient.runPromise(Pglite),
+  errorComponent: () => <p>Error</p>,
+});
+
+function RootComponent() {
+  const { client, orm } = Route.useLoaderData();
+  return (
+    <PGliteProvider db={client}>
+      <PgliteDrizzleContext.Provider value={orm}>
+        <div className="max-w-xl mx-auto pt-12 bg-theme-background">
+          <Outlet />
+        </div>
+      </PgliteDrizzleContext.Provider>
+    </PGliteProvider>
+  );
+}
