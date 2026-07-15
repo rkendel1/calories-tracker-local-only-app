9cead2d0e611d9de579507ea98d9b0e0f479d5cc
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-07-15 15:44:03 -0400

    Adopt repository: src/components/UpdateFood.tsx

---
+++ src/components/UpdateFood.tsx
@@ -0,0 +1,52 @@
+import { useMachine } from "@xstate/react";
+import { UserPenIcon } from "lucide-react";
+import { machine } from "~/machines/manage-food";
+import type { FoodSelect } from "~/schema/food";
+import { Button } from "./Button";
+import { Dialog, DialogTrigger } from "./Dialog";
+import FoodEditing from "./FoodEditing";
+import { Modal, ModalOverlay } from "./Modal";
+
+export default function UpdateFood({ food }: { food: FoodSelect }) {
+  const [snapshot, send] = useMachine(machine, { input: food });
+  return (
+    <DialogTrigger>
+      <Button>
+        <UserPenIcon size={16} />
+      </Button>
+      <ModalOverlay isDismissable>
+        <Modal>
+          <Dialog>
+            {({ close }) => (
+              <FoodEditing
+                brandActor={snapshot.context.brand}
+                caloriesActor={snapshot.context.calories}
+                carbohydratesActor={snapshot.context.carbohydrates}
+                fibersActor={snapshot.context.fibers}
+                nameActor={snapshot.context.name}
+                proteinsActor={snapshot.context.proteins}
+                saltActor={snapshot.context.salt}
+                sugarsActor={snapshot.context.sugars}
+                fatsActor={snapshot.context.fats}
+                fatsSaturatedActor={snapshot.context.fatsSaturated}
+                onSubmit={() => send({ type: "food.update", id: food.id })}
+              >
+                <Button
+                  action="update"
+                  type="submit"
+                  isDisabled={snapshot.matches("Updating")}
+                >
+                  {snapshot.matches("Updating") ? "Updating..." : "Update"}
+                </Button>
+
+                {snapshot.context.submitError !== null && (
+                  <p>{snapshot.context.submitError}</p>
+                )}
+              </FoodEditing>
+            )}
+          </Dialog>
+        </Modal>
+      </ModalOverlay>
+    </DialogTrigger>
+  );
+}
